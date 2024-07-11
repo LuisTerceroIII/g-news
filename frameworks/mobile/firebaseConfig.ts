0186bb7a79b5522f5d06e5650243131d14c88d13
Luis Espinoza <luis.espinoza.nav@live.com>
2024-07-11 02:37:25 -0300

    update firebase files

--- frameworks/mobile/firebaseConfig.ts
+++ frameworks/mobile/firebaseConfig.ts
@@ -1,7 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,18 +7,15 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDp5DiHkBlDvfC5isKUUTmq2ZP4eGoUs8k",
-  authDomain: "news-board-d605b.firebaseapp.com",
-  projectId: "news-board-d605b",
-  storageBucket: "news-board-d605b.appspot.com",
-  messagingSenderId: "21756226705",
-  appId: "1:21756226705:web:eaf4180f9963bc8bab2330",
-  measurementId: "G-3P9H9GSFCH"
+  apiKey: process.env.AIzaSyDp5DiHkBlDvfC5isKUUTmq2ZP4eGoUs8k,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
